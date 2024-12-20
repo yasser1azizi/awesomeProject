1545b5e05b819d6a05a8f3f6b3466d510eeb02d0
yasser1azizi <yaaser72@gmail.com>
2024-12-20 13:56:34 +0100

    simple_code

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+  "fmt"
+  "log"
+  "net/http"
+)
+
+func main() {
+  http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "Hello, Secure World!")
+  })
+
+  port := "8080"
+  log.Printf("Starting server on port %s...", port)
+  if err := http.ListenAndServe(":"+port, nil); err != nil {
+    log.Fatalf("Server failed: %s", err)
+  }
+}
