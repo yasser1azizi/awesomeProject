4906f0764e4a77e82cf7d3407abbb03fb22e027b
yasser1azizi <yaaser72@gmail.com>
2024-12-23 22:05:53 +0100

    sec_tools_test-2

--- main.go
+++ main.go
@@ -1,19 +1,24 @@
 package main
 
 import (
-  "fmt"
-  "log"
-  "net/http"
+	"fmt"
+	"log"
+	"net/http"
 )
 
 func main() {
-  http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Hello, Secure World!")
-  })
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, Secure World!")
+	})
 
-  port := "8080"
-  log.Printf("Starting server on port %s...", port)
-  if err := http.ListenAndServe(":"+port, nil); err != nil {
-    log.Fatalf("Server failed: %s", err)
+	port := "8080"
+	log.Printf("Starting server on port %s...", port)
+	if err := http.ListenAndServe(":"+port, nil); err != nil {
+		log.Fatalf("Server failed: %s", err)
+	}
+  func doNothing() {
+    fmt.Println("This function does nothing.")
+    //^_^
   }
+
 }
